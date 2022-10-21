49ee37d60938941ca7cf5f3dae24cb22a2f612fb
Namgel <namgeltamang14@gmail.com>
2022-10-21 20:45:56 +0545

    Add mobile responsiveness.

--- js/app.js
+++ js/app.js
@@ -25,3 +25,11 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
         });
     });
 });
+
+// Responsive navbar
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("nav-links")[0];
+
+toggleButton.addEventListener("click", () => {
+    navbarLinks.classList.toggle("active");
+});
